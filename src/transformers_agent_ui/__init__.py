56e7e02eae6edd3290291782d99a66262f9cead1
Your Name <INITIALS@orsted.com>
2023-05-15 02:37:37 +0000

    update to version 0.4.0

--- src/transformers_agent_ui/__init__.py
+++ src/transformers_agent_ui/__init__.py
@@ -4,5 +4,5 @@ A component you can use in the notebook or your (Panel) web app.
 """
 from transformers_agent_ui.ui.transformers_agent_ui import TransformersAgentUI
 
-VERSION = "0.3.0"
+VERSION = "0.4.0"
 __all__ = ["TransformersAgentUI"]
